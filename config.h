925c7e6787175d8aa34987def3ddb65ae34a3715
will@gmail.com <will@gmail.com>
2022-07-22 14:26:29 -0500

    better colors; xfce4-terminal

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#51ed32";
-static const char col_gray4[]       = "#51ed32";
-static const char col_cyan[]        = "#162914";
+static const char col_gray4[]       = "#222222";
+static const char col_cyan[]        = "#51ed32";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "xfce4-terminal", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
 
 static Key keys[] = {
